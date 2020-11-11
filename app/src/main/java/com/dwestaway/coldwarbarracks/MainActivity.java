a79f361a6bbfb581252e798190f905e4bc6461ec
dwestaway <danwesty1@googlemail.com>
2020-11-11 11:17:42 +0000

    Layout changes
    Removed countdown, made back arrow bigger

--- app/src/main/java/com/dwestaway/coldwarbarracks/MainActivity.java
+++ app/src/main/java/com/dwestaway/coldwarbarracks/MainActivity.java
@@ -37,9 +37,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-        final TextView tv_countdown = findViewById(R.id.countdownText);
-
         final Button primaryWeaponsButton = findViewById(R.id.primaryWeaponsButton);
         final Button secondaryWeaponsButton = findViewById(R.id.secondaryWeaponsButton);
         final Button equipmentButton = findViewById(R.id.equipmentButton);
@@ -155,40 +152,9 @@ public class MainActivity extends AppCompatActivity {
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
-        Calendar start_calendar = Calendar.getInstance();
-        Calendar end_calendar = Calendar.getInstance();
-
-        end_calendar.set(2020, 10, 13); // 10 = November, month start at 0 = January
-
-        long start_millis = start_calendar.getTimeInMillis(); //get the start time in milliseconds
-        long end_millis = end_calendar.getTimeInMillis(); //get the end time in milliseconds
-        long total_millis = (end_millis - start_millis); //total time in milliseconds
 
 
-        //1000 = 1 second interval
-        CountDownTimer cdt = new CountDownTimer(total_millis, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                long days = TimeUnit.MILLISECONDS.toDays(millisUntilFinished);
-                millisUntilFinished -= TimeUnit.DAYS.toMillis(days);
 
-                long hours = TimeUnit.MILLISECONDS.toHours(millisUntilFinished);
-                millisUntilFinished -= TimeUnit.HOURS.toMillis(hours);
-
-                long minutes = TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished);
-                millisUntilFinished -= TimeUnit.MINUTES.toMillis(minutes);
-
-                long seconds = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished);
-
-                tv_countdown.setText(days + " days " + hours + " hours\n" + minutes + " minutes " + seconds + " seconds"); //You can compute the millisUntilFinished on hours/minutes/seconds
-            }
-
-            @Override
-            public void onFinish() {
-                tv_countdown.setText("Finish!");
-            }
-        };
-        cdt.start();
     }
 
 
